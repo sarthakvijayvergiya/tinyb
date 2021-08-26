fd7e048d6ca284d34bfddf1aa091ee7c59ff3cc5
Sarthak Vijayvergiya <sarthakvijayvergiya@gmail.com>
2021-08-27 03:46:43 +0530

    Update HelloTinyB.java

--- examples/java/HelloTinyB.java
+++ examples/java/HelloTinyB.java
@@ -180,7 +180,7 @@ public class HelloTinyB {
         }
         System.out.println("Found service " + tempService.getUUID());
 
-        BluetoothGattCharacteristic tempValue = getCharacteristic(tempService, args[1]);
+        BluetoothGattCharacteristic tempValue = getCharacteristic(tempService, args[2]);
         System.out.println("Found BluetoothGattCharacteristic " + tempValue.getUUID());
 //         BluetoothGattCharacteristic tempConfig = getCharacteristic(tempService, "f000aa02-0451-4000-b000-000000000000");
 //         BluetoothGattCharacteristic tempPeriod = getCharacteristic(tempService, "f000aa03-0451-4000-b000-000000000000");
