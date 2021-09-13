401587a9fcea6a96f60774329dd98f7e36843551
Sarthak Vijayvergiya <sarthakvijayvergiya@jklu.edu.in>
2021-09-14 03:17:22 +0530

    Update HelloTinyB.java

--- examples/java/HelloTinyB.java
+++ examples/java/HelloTinyB.java
@@ -252,7 +252,7 @@ public class HelloTinyB {
     for (byte b : calibrationValue) {
        System.out.print(String.format("%02x,", b));
      }
-       int temperature = ByteBuffer.wrap(calibrationValue).order(ByteOrder.LITTLE_ENDIAN).getInt();
+       int temperature = ByteBuffer.wrap(calibrationValue).getInt();
 //     double temperature =
 //         ByteBuffer.wrap(calibrationValue).order(ByteOrder.LITTLE_ENDIAN).getDouble();
 
